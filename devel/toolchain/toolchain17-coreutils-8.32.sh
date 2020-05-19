95deff7a0bc764ab20544d1be1daa5e366cd4c8c
Fabian Theimert <fabian@theimert.net>
2020-05-19 09:54:20 +0200

    fix and stage2 script

--- devel/toolchain/toolchain17-coreutils-8.32.sh
+++ devel/toolchain/toolchain17-coreutils-8.32.sh
@@ -8,5 +8,5 @@ cd $LFS/sources/coreutils-8.32 &&
 
 ./configure --prefix=/tools --enable-install-program=hostname &&
 make &&
-make RUN_EXPENSIVE_TESTS=yes check &&
+make -j1 RUN_EXPENSIVE_TESTS=yes check  &&
 make install
