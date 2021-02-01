6b1fdab5b2f53d2b96755aeb11d8558055e77267
Fabian Theimert <kosmonaut@theimert.net>
2021-02-01 11:27:06 +0100

    missing config

---
+++ config/config10_lsb.sh
@@ -0,0 +1,14 @@
+cat > /etc/os-release << "EOF"
+NAME="Kilix"
+VERSION="20210201-systemd"
+ID=Killix
+PRETTY_NAME="Based on Linux From Scratch"
+VERSION_CODENAME="Trails"
+EOF
+
+cat > /etc/lsb-release << "EOF"
+DISTRIB_ID="Kilix"
+DISTRIB_RELEASE="20210201-systemd"
+DISTRIB_CODENAME="Fabian Theimert"
+DISTRIB_DESCRIPTION="Based on Linux From Scratch"
+EOF
